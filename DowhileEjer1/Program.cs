1b26f702d00b44aaf76530f85dbe62c5c9a749a3
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-03-10 04:10:18 -0500

    Agregar archivos de proyecto.

---
+++ DowhileEjer1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DowhileEjer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0,
+            contador = 1,
+            n = 1,
+            suma = 0,
+            i = 0;
+
+
+            while (contador < 100)
+
+            {
+                suma = suma + num1;
+                Console.WriteLine(suma);
+                Console.WriteLine(contador);
+                contador = contador + 1;
+            }
+            Console.ReadKey();
+        }
+       
+        
+    }
+}
